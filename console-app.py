6a6be0f722e8b7da696f74eff55f507f1a1e3b95
Shaun Nguyen <83482797+Shaun7heSheep@users.noreply.github.com>
2023-03-01 20:47:42 -0800

    add console-app.py, requires python 3.10 and above

---
+++ console-app.py
@@ -0,0 +1,21 @@
+# Python 3.10
+import requests
+
+while True:
+    req = input("Command: ")
+    arr = req.split(',')
+    cmd = arr[0]
+    match cmd: # Only Python 3.10 and above support this
+        case 'exit':
+            break
+        case 'ADD':
+            response = requests.post(
+                'http://localhost/add_user',
+                json = {"name": arr[1], "age": arr[2]}
+            )
+            print(response.json())
+        case 'QUOTE':
+            response = requests.get(
+                f'http://localhost/quote?userID={arr[1]}&symbol={arr[2]}'
+            )
+            print(response.json())
