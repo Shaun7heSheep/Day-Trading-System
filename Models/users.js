49f8e8131f77a03949b547acc189baf7a04b9b51
xianyao009 <yao009@hotmail.com>
2023-03-03 19:16:09 -0800

    Added new properties into UserSchema

--- Models/users.js
+++ Models/users.js
@@ -1,15 +1,62 @@
 const mongoose = require("mongoose");
 
-const UserSchema = new mongoose.Schema({
-  userID: {
-    type: String,
-    required: true,
+const UserSchema = new mongoose.Schema(
+  {
+    userID: {
+      type: String,
+      required: true,
+    },
+    balance: {
+      type: Number,
+      default: 0,
+    },
+    stocksOwned: [
+      {
+        symbol: {
+          type: String,
+          required: true,
+        },
+        quantity: {
+          type: Number,
+          required: true,
+          default: 0,
+        },
+      },
+    ],
+    buyTriggers: [
+      {
+        symbol: {
+          type: String,
+          required: true,
+        },
+        amount: {
+          type: Number,
+          required: true,
+        },
+        trigger_Price: {
+          type: Number,
+          required: true,
+        },
+      },
+    ],
+    sellTriggers: [
+      {
+        symbol: {
+          type: String,
+          required: true,
+        },
+        amount: {
+          type: Number,
+          required: true,
+        },
+        trigger_Price: {
+          type: Number,
+          required: true,
+        },
+      },
+    ],
   },
-  balance: {
-    type: Number,
-    default: 0,
-  },
-}, { timestamps: true }
+  { timestamps: true }
 );
 
 const User = mongoose.model("User", UserSchema);
