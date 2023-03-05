65ae48974bc25dac508fb2ea4f9bf98e9ffcbcb8
xianyao009 <yao009@hotmail.com>
2023-03-04 18:38:13 -0800

    Refactored file structure and separated routes and controller

--- server.js
+++ server.js
@@ -1,39 +1,32 @@
 // simple localhost server
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const mongoose = require("mongoose");
-const userRouter = require("./Routes/users")
-const transactionRouter = require("./Routes/transactions")
-const dumplog = require("./Routes/dumplog")
+const userRoutes = require("./Routes/userRoutes");
+const transactionRouter = require("./Routes/transactions");
+const dumplog = require("./Routes/dumplog");
 
 const app = express();
-app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, '/index.html'));
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "/index.html"));
 });
-
 
 app.use(express.json());
 
 // Connect to DB
-mongoose.connect('mongodb://127.0.0.1:27017/seng468db',
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  }
-);
+mongoose.connect("mongodb://127.0.0.1:27017/seng468db", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 // Test DB connection
 const db = mongoose.connection;
-db.on("error", console.error.bind(console,"connection error: "));
-db.once("open", function() {
-    console.log("Connected to MongoDB successfully");
+db.on("error", console.error.bind(console, "connection error: "));
+db.once("open", function () {
+  console.log("Connected to MongoDB successfully");
 });
 
-app.use(userRouter);
+app.use("/", userRoutes);
 app.use(transactionRouter);
 app.use(dumplog);
 
-app.listen(3000, () => console.log('Server is up and running'));
-
-
-
-
+app.listen(3000, () => console.log("Server is up and running"));
