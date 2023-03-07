3e54ad9ef29c4bd1c359bcdd6c31145612e07b22
Shaun 7he Sheep <snguyendohoang@gmail.com>
2023-03-06 22:50:11 -0800

    updated to work on lab machine

--- Controllers/quoteController.js
+++ Controllers/quoteController.js
@@ -1,17 +1,19 @@
 const net = require("net");
 const logController = require("./logController");
+const transactionNumController = require("./transactNumController");
 
 exports.getStockPrice = async (request, response) => {
   // get and update current transactionNum
   var numDoc = await transactionNumController.getNextTransactNum()
   // log user command
-  logController.logUserCmnd("ADD",request,numDoc.value);
+  logController.logUserCmnd("QUOTE",request,numDoc.value);
 
   let userID = request.query.user_id;
   let symbol = request.query.symbol;
 
+
   try {
-    quoteData = await getQuote(userID, symbol, numDoc.value);
+    quoteData = await this.getQuote(userID, symbol, numDoc.value);
     response.status(200).send(quoteData);
   } catch (error) {
     response.status(500).send(error);
@@ -26,12 +28,13 @@ exports.getQuote = (userID, symbol, transactionNum) => {
       port: 4444,
     });
     client.on("connect", () => {
+      console.log("Connected to quoteserver");
       client.write(`${symbol},${userID}\n`);
     });
     client.on("data", (data) => {
       var response = data.toString("utf-8");
       resolve(response);
-      // var arr = response.split(",");
+      var arr = response.split(",");
       // store quoteserver response for logging
       logController.logQuoteServer(userID,symbol,arr[0],arr[3],arr[4], transactionNum);
     });
