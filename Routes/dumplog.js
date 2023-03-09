20fd3891bfd084796cf86d0c3277ed54d2cba7e3
Shaun <snguyendohoang@gmail.com>
2023-03-09 14:32:19 -0800

    change app to router

--- Routes/dumplog.js
+++ Routes/dumplog.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const logController = require("../Controllers/logController");
 
-app.get("/dump", logController.dumplog);
+router.get("/dump", logController.dumplog);
 
 // Route for deleting all the user log
 router.delete("/dump", logController.deleteAllLog);
