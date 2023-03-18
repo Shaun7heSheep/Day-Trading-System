b7156bf23e0f2304b13a4bcd93ef2d44518f5228
Shaun <snguyendohoang@gmail.com>
2023-03-18 00:11:48 -0700

    make api path dynamic

--- command_script.js
+++ command_script.js
@@ -1,77 +1,78 @@
-const fs = require('fs');
-const axios = require('axios');
+const fs = require(`fs`);
+const axios = require(`axios`);
 
-const FILE_PATH = 'user10.txt';
+const FILE_PATH = `user10.txt`;
+const API_PATH = `http://localhost/seng468/`;
 
-// axios.delete("http://localhost:3000/users");
-// axios.delete("http://localhost:3000/transactions");
-// axios.delete("http://localhost:3000/dump");
-fs.readFile(FILE_PATH, 'utf8', async (err, data) => {
+// axios.delete(`${API_PATH}users`);
+// axios.delete(`${API_PATH}transactions`);
+// axios.delete(`${API_PATH}dump`);
+fs.readFile(FILE_PATH, `utf8`, async (err, data) => {
     if (err) {
         console.error(err);
         return;
     }
 
-    const lines = data.trim().split('\n');
+    const lines = data.trim().split(`\n`);
     for (let i = 0; i < lines.length; i++) {
-        const [first, ...args] = lines[i].trim().split(',');
+        const [first, ...args] = lines[i].trim().split(`,`);
         const command = first.split(/[\s]+/)[1];
 
-        console.log("Transaction: " + i);
+        console.log(`Transaction: ` + i);
         try {
             switch (command) {
-                case 'ADD':
-                    await axios.post("http://localhost:3000/users", { userID: args[0], amount: args[1] });
+                case `ADD`:
+                    await axios.post(`${API_PATH}users`, { userID: args[0], amount: args[1] });
                     break;
-                case 'QUOTE':
-                    await axios.get(`http://localhost:3000/quote`, {
+                case `QUOTE`:
+                    await axios.get(`${API_PATH}quote`, {
                         data: {
                             user_id: args[0],
                             symbol: args[1]
                         }
                     });
                     break;
-                case 'BUY':
-                    await axios.post('http://localhost:3000/buy', { userID: args[0], symbol: args[1], amount: args[2] });
+                case `BUY`:
+                    await axios.post(`${API_PATH}buy`, { userID: args[0], symbol: args[1], amount: args[2] });
                     break;
-                case 'COMMIT_BUY':
-                    await axios.post('http://localhost:3000/commit_buy', { userID: args[0] });
+                case `COMMIT_BUY`:
+                    await axios.post(`${API_PATH}commit_buy`, { userID: args[0] });
                     break;
-                case "CANCEL_BUY":
-                    await axios.post('http://localhost:3000/cancel_buy', { userID: args[0] });
+                case `CANCEL_BUY`:
+                    await axios.post(`${API_PATH}cancel_buy`, { userID: args[0] });
                     break;
-                case "SELL":
-                    await axios.post('http://localhost:3000/sell', { userID: args[0], symbol: args[1], amount: args[2] });
+                case `SELL`:
+                    await axios.post(`${API_PATH}sell`, { userID: args[0], symbol: args[1], amount: args[2] });
                     break;
-                case "COMMIT_SELL":
-                    await axios.post('http://localhost:3000/commit_sell', { userID: args[0] });
+                case `COMMIT_SELL`:
+                    await axios.post(`${API_PATH}commit_sell`, { userID: args[0] });
                     break;
-                case "CANCEL_SELL":
-                    await axios.post('http://localhost:3000/cancel_sell', { userID: args[0] });
+                case `CANCEL_SELL`:
+                    await axios.post(`${API_PATH}cancel_sell`, { userID: args[0] });
                     break;
-                case "SET_BUY_AMOUNT":
-                    await axios.post('http://localhost:3000/users/set-buy-amount', { userID: args[0], symbol: args[1], amount: args[2] });
+                case `SET_BUY_AMOUNT`:
+                    await axios.post(`${API_PATH}users/set-buy-amount`, { userID: args[0], symbol: args[1], amount: args[2] });
                     break;
-                case "CANCEL_SET_BUY":
-                    await axios.post('http://localhost:3000/users/cancel-set-buy', { userID: args[0], symbol: args[1] });
+                case `CANCEL_SET_BUY`:
+                    await axios.post(`${API_PATH}users/cancel-set-buy`, { userID: args[0], symbol: args[1] });
                     break;
-                case "SET_BUY_TRIGGER":
-                    await axios.post('http://localhost:3000/users/set-buy-trigger', { userID: args[0], symbol: args[1], amount: args[2] });
+                case `SET_BUY_TRIGGER`:
+                    await axios.post(`${API_PATH}users/set-buy-trigger`, { userID: args[0], symbol: args[1], amount: args[2] });
                     break;
-                case "SET_SELL_AMOUNT":
-                    await axios.post('http://localhost:3000/users/set-sell-amount', { userID: args[0], symbol: args[1], amount: args[2] });
+                case `SET_SELL_AMOUNT`:
+                    await axios.post(`${API_PATH}users/set-sell-amount`, { userID: args[0], symbol: args[1], amount: args[2] });
                     break;
-                case "SET_SELL_TRIGGER":
-                    await axios.post('http://localhost:3000/users/set-sell-trigger', { userID: args[0], symbol: args[1], amount: args[2] });
+                case `SET_SELL_TRIGGER`:
+                    await axios.post(`${API_PATH}users/set-sell-trigger`, { userID: args[0], symbol: args[1], amount: args[2] });
                     break;
-                case "CANCEL_SET_SELL":
-                    await axios.post('http://localhost:3000/users/cancel-set-sell', { userID: args[0], symbol: args[1] });
+                case `CANCEL_SET_SELL`:
+                    await axios.post(`${API_PATH}users/cancel-set-sell`, { userID: args[0], symbol: args[1] });
                     break;
-                case "DUMPLOG":
-                    await axios.get('http://localhost:3000/dump', { data: { filename: args[0] } });
+                case `DUMPLOG`:
+                    await axios.get(`${API_PATH}dump`, { data: { filename: args[0] } });
                     break;
-                case "DISPLAY_SUMMARY":
-                    await axios.post('http://localhost:3000/display_summary', { userID: args[0] });
+                case `DISPLAY_SUMMARY`:
+                    await axios.post(`${API_PATH}display_summary`, { userID: args[0] });
                     break;
                 default:
                     console.warn(`Unknown command: ${command}`);
