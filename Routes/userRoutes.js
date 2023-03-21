814bae60156b3e67ce6b942779fd614156b5f6f4
VietNgo20 <hoangviet06102k@gmail.com>
2023-03-21 11:02:42 -0700

    remove 'users' in the routes and script

--- Routes/userRoutes.js
+++ Routes/userRoutes.js
@@ -18,21 +18,21 @@ router.put("/users/:userID", userController.updateUserByUserId);
 router.delete("/users", userController.deleteAllUsers);
 
 // Route for setting the buy amount
-router.post("/users/set-buy-amount", userController.setBuyAmount);
+router.post("/set-buy-amount", userController.setBuyAmount);
 
 // Route for setting the buy trigger
-router.post("/users/set-buy-trigger", userController.setBuyTrigger);
+router.post("/set-buy-trigger", userController.setBuyTrigger);
 
 // Route for cancelling the SET_BUY commands
-router.post("/users/cancel-set-buy", userController.cancelSetBuy);
+router.post("/cancel-set-buy", userController.cancelSetBuy);
 
 // Route for setting the sell amount
-router.post("/users/set-sell-amount", userController.setSellAmount);
+router.post("/set-sell-amount", userController.setSellAmount);
 
 // Route for setting the sell trigger
-router.post("/users/set-sell-trigger", userController.setSellTrigger);
+router.post("/set-sell-trigger", userController.setSellTrigger);
 
 // Route for cancelling the SET_SELL commands
-router.post("/users/cancel-set-sell", userController.cancelSetSell);
+router.post("/cancel-set-sell", userController.cancelSetSell);
 
 module.exports = router;
