d37f9ffa948cc54365960377a37ee464f7e370c2
Shaun Nguyen <83482797+Shaun7heSheep@users.noreply.github.com>
2023-03-27 20:21:33 -0700

    update script

--- command_script2.js
+++ command_script2.js
@@ -6,15 +6,19 @@ const API_PATH = `http://localhost/seng468/`;
 
 const requestQueue = [];
 const maxQueueSize = process.argv[3] || 1;
+const unresolvedPromises = {};
 
 async function sendRequest(command, args) {
+    while (unresolvedPromises[args[0]] == 1) {
+        await Promise.race(requestQueue);
+    }
     try {
         switch (command) {
             case `ADD`:
-                await axios.post(`${API_PATH}users`, { userID: args[0], amount: args[1] });
+                return await axios.post(`${API_PATH}users`, { userID: args[0], amount: args[1] });
                 break;
             case `QUOTE`:
-                await axios.get(`${API_PATH}quote`, {
+                return await axios.get(`${API_PATH}quote`, {
                     data: {
                         user_id: args[0],
                         symbol: args[1]
@@ -22,46 +26,46 @@ async function sendRequest(command, args) {
                 });
                 break;
             case `BUY`:
-                await axios.post(`${API_PATH}buy`, { userID: args[0], symbol: args[1], amount: args[2] });
+                return await axios.post(`${API_PATH}buy`, { userID: args[0], symbol: args[1], amount: args[2] });
                 break;
             case `COMMIT_BUY`:
-                await axios.post(`${API_PATH}commit_buy`, { userID: args[0] });
+                return await axios.post(`${API_PATH}commit_buy`, { userID: args[0] });
                 break;
             case `CANCEL_BUY`:
-                await axios.post(`${API_PATH}cancel_buy`, { userID: args[0] });
+                return await axios.post(`${API_PATH}cancel_buy`, { userID: args[0] });
                 break;
             case `SELL`:
-                await axios.post(`${API_PATH}sell`, { userID: args[0], symbol: args[1], amount: args[2] });
+                return await axios.post(`${API_PATH}sell`, { userID: args[0], symbol: args[1], amount: args[2] });
                 break;
             case `COMMIT_SELL`:
-                await axios.post(`${API_PATH}commit_sell`, { userID: args[0] });
+                return await axios.post(`${API_PATH}commit_sell`, { userID: args[0] });
                 break;
             case `CANCEL_SELL`:
-                await axios.post(`${API_PATH}cancel_sell`, { userID: args[0] });
+                return await axios.post(`${API_PATH}cancel_sell`, { userID: args[0] });
                 break;
             case `SET_BUY_AMOUNT`:
-                await axios.post(`${API_PATH}set-buy-amount`, { userID: args[0], symbol: args[1], amount: args[2] });
+                return await axios.post(`${API_PATH}set-buy-amount`, { userID: args[0], symbol: args[1], amount: args[2] });
                 break;
             case `CANCEL_SET_BUY`:
-                await axios.post(`${API_PATH}cancel-set-buy`, { userID: args[0], symbol: args[1] });
+                return await axios.post(`${API_PATH}cancel-set-buy`, { userID: args[0], symbol: args[1] });
                 break;
             case `SET_BUY_TRIGGER`:
-                await axios.post(`${API_PATH}set-buy-trigger`, { userID: args[0], symbol: args[1], amount: args[2] });
+                return await axios.post(`${API_PATH}set-buy-trigger`, { userID: args[0], symbol: args[1], amount: args[2] });
                 break;
             case `SET_SELL_AMOUNT`:
-                await axios.post(`${API_PATH}set-sell-amount`, { userID: args[0], symbol: args[1], amount: args[2] });
+                return await axios.post(`${API_PATH}set-sell-amount`, { userID: args[0], symbol: args[1], amount: args[2] });
                 break;
             case `SET_SELL_TRIGGER`:
-                await axios.post(`${API_PATH}set-sell-trigger`, { userID: args[0], symbol: args[1], amount: args[2] });
+                return await axios.post(`${API_PATH}set-sell-trigger`, { userID: args[0], symbol: args[1], amount: args[2] });
                 break;
             case `CANCEL_SET_SELL`:
-                await axios.post(`${API_PATH}cancel-set-sell`, { userID: args[0], symbol: args[1] });
+                return await axios.post(`${API_PATH}cancel-set-sell`, { userID: args[0], symbol: args[1] });
                 break;
             case `DUMPLOG`:
-                await axios.get(`${API_PATH}dump`, { data: { filename: args[0] } });
+                return await axios.get(`${API_PATH}dump`, { data: { filename: args[0] } });
                 break;
             case `DISPLAY_SUMMARY`:
-                await axios.post(`${API_PATH}display_summary`, { userID: args[0] });
+                return await axios.post(`${API_PATH}display_summary`, { userID: args[0] });
                 break;
             default:
                 console.warn(`Unknown command: ${command}`);
@@ -69,6 +73,7 @@ async function sendRequest(command, args) {
         }
     } catch (err) {
         console.error(`Error sending command ${command}: ${err.message}`);
+        return err
     }
 };
 
