a37651bc65c8102cf4a703ff630089e4ce846e8a
Shaun Nguyen <83482797+Shaun7heSheep@users.noreply.github.com>
2023-03-30 18:23:28 -0700

    caching stockPrice in getQuote function

--- transactionServer/Controllers/quoteController.js
+++ transactionServer/Controllers/quoteController.js
@@ -1,4 +1,5 @@
 const net = require("net");
+const cache = require("../Redis/redis_init");
 const logController = require("./logController");
 const transactionNumController = require("./transactNumController");
 
@@ -6,10 +7,10 @@ exports.getStockPrice = async (request, response) => {
   // get and update current transactionNum
   var numDoc = await transactionNumController.getNextTransactNum()
   // log user command
-  logController.logUserCmnd("QUOTE",request,numDoc.value);
+  logController.logUserCmnd("QUOTE", request, numDoc.value);
 
   try {
-    quoteData = await this.getQuote(request.body.user_id, request.body.symbol, numDoc.value);
+    quoteData = await this.getQuote(request.body.user_id, request.body.symbol, numDoc);
     response.status(200).send(quoteData);
   } catch (error) {
     response.status(500).send(error);
@@ -18,24 +19,37 @@ exports.getStockPrice = async (request, response) => {
 
 // Connect to QuoteServer and get quote
 exports.getQuote = (userID, symbol, transactionNum) => {
-  return new Promise((resolve, reject) => {
-    const client = net.createConnection({
-      host: "quoteserve.seng.uvic.ca",
-      port: 4444,
-    });
-    client.on("connect", () => {
-      //console.log("Connected to quoteserver");
-      client.write(`${symbol},${userID}\n`);
-    });
-    client.on("data", async (data) => {
-      var response = data.toString("utf-8");
-      resolve(response);
-      var arr = response.split(",");
-      // store quoteserver response for logging
-      await logController.logQuoteServer(userID,symbol,arr[0],arr[3],arr[4], transactionNum);
-    });
-    client.on("error", (err) => {
-      reject(err);
-    });
+  return new Promise(async (resolve, reject) => {
+    // get stock price in cache
+    var stockCached = await cache.get(symbol);
+    if (!stockCached) {
+      const client = net.createConnection({
+        host: "quoteserve.seng.uvic.ca",
+        port: 4444,
+      });
+      client.on("connect", () => {
+        client.write(`${symbol},${userID}\n`);
+      });
+      client.on("data", async (data) => {
+        var response = data.toString("utf-8");
+        var arr = response.split(",");
+        const stockObj = {
+          price: Number(arr[0]),
+          quoteTime: arr[3],
+          cryptoKey: arr[4]
+        };
+        // cache stock price
+        cache.set(symbol,JSON.stringify(stockObj));
+        resolve(stockObj);
+      });
+      client.on("error", (err) => {
+        reject(err);
+      });
+    } else {
+      const stockObj = JSON.parse(stockPrice);
+      resolve(stockObj);
+    }
+    // store quoteserver response for logging
+    logController.logQuoteServer(userID, symbol, stockObj.price, stockObj.quoteTime, stockObj.cryptoKey, transactionNum);
   });
 }
