a398a9b888f388e81cbc89d3503dc713cf2d3b59
xianyao009 <yao009@hotmail.com>
2023-04-02 20:23:02 -0700

    installed ejs

--- transactionServer/server.js
+++ transactionServer/server.js
@@ -2,20 +2,30 @@
 const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
+const ejs = require("ejs");
 const userRoutes = require("./Routes/userRoutes");
 const transactionRoutes = require("./Routes/transactionRoutes");
 const quoteRoutes = require("./Routes/quoteRoutes");
 const dumplog = require("./Routes/dumplog");
 
 const app = express();
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views")
 const port = process.env.PORT || 3000;
 const dbString = process.env.MONGODB_CONNSTRING || 'mongodb://localhost:60000/seng468db';
 
 require("./Redis/redis_init")
 
-app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname, "/index.html"));
-});
+// app.get("/", function (req, res) {
+//   res.sendFile(path.join(__dirname, "/index.html"));
+// });
+
+app.get("/", (req, res) => {
+  res.render("index", {
+    title: "This is the title",
+    message: "This is the message"
+  })
+})
 
 app.use(express.json());
 
